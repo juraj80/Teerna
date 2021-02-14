4d53815a33f6f0ca0af95d8bc567d9a6d67429a0
ndvo <nelson@ocastudios.com>
2021-02-13 21:12:24 -0300

    feat(auth): get user data from firebase token

--- server/index.js
+++ server/index.js
@@ -1,12 +1,15 @@
-const express = require('express');
+const authenticate = require('./auth.js');
 const bodyParser = require('body-parser');
-
 const chat = require('./Chat/Chat.js');
+const express = require('express');
+
 
 const port = 8000;
 
+// Creates the Express App
 const app = express();
 
+// The API routes
 const apiRouter = require('./API');
 
 /**
@@ -16,21 +19,22 @@ const apiRouter = require('./API');
  */
 app.use('/', express.static('../client/build'));
 
-/**
- * Body parser for handling POST requests.
- */
+// Body parser for handling POST requests.
 app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
-
 
 /**
  * Backend API
  */
-app.use('/api', apiRouter);
+app.use('/api', authenticate, apiRouter);
 
-
+// Start the HTTP server
 app.listen(port, () => {
   console.log(`Teerna Server listening on port`, port, `!`)
 });
 
+// Setup the WebSocket server
 chat.setUpChat();
+
+// Export for testing purposes
+module.exports = app

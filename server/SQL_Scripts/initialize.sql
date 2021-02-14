35d5136ed1585a658c4000eba58a0ddc2ea6f7ed
ndvo <nelson@ocastudios.com>
2021-02-13 21:14:16 -0300

    feat(player): add email to playerList table
    Email is needed to send invitations and to keep track of invited players.

--- server/SQL_Scripts/initialize.sql
+++ server/SQL_Scripts/initialize.sql
@@ -1,6 +1,6 @@
 CREATE TABLE battleMap (tokenID INTEGER NOT NULL PRIMARY KEY, tokenName VARCHAR(100), tokenPosX INT, tokenPosY INT);
 
-CREATE TABLE playerList (playerID INTEGER NOT NULL PRIMARY KEY, isGM BOOL, playerName VARCHAR(20));
+CREATE TABLE playerList (playerID INTEGER NOT NULL PRIMARY KEY, isGM BOOL, playerName VARCHAR(20), email VARCHAR(256));
 
 CREATE TABLE fileList (fileID INTEGER NOT NULL PRIMARY KEY, fileName VARCHAR(100), fileLocation VARCHAR(100), permissionID int);
 

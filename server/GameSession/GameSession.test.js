e388c264ae1e51ccb74bf61feef9a753636da9bb
ndvo <nelson@ocastudios.com>
2021-03-25 09:54:29 -0300

    test(gamesession): create and remove games

--- server/GameSession/GameSession.test.js
+++ server/GameSession/GameSession.test.js
@@ -21,47 +21,52 @@ describe("Game Session Class", function() {;
   describe("Create Game Sessions", function() {
     it("Should require user_id and email to create a session.", async function() {
       expect(()=>GameSession.createSession({})).to.throw;
-      expect(()=>GameSession.createSession({user_id: 'foo', email: 'bar'})).not.to.throw;
+      expect(()=>GameSession.createSession({user_id: 'gm', email: 'gamemaster@example.com'})).not.to.throw;
     });
 
     it("Should create a new game id for a newly created game", async function() {
-      const game = await GameSession.createSession({user_id: 'foo', email: 'bar'});
       expect(game.guid).to.exist;
       expect(game.guid.length).to.equal(36);
       expect(game.guid.split('-').length).to.equal(5);
-      const game2 = await GameSession.createSession({user_id: 'foo', email: 'bar'});
+      const game2 = await GameSession.createSession({user_id: 'gm', email: 'gamemaster@example.com'});
       expect(game2.guid).to.exist;
       expect(game2.guid.length).to.equal(36);
       expect(game2.guid.split('-').length).to.equal(5);
       expect(game.guid).not.to.equal(game2.guid);
+      game2.deleteGame();
     });
 
     it("Should create a new database file name for a newly created game", async function() {
-      const game = await GameSession.createSession({user_id: 'foo', email: 'bar'});
       expect(game.dbFile).to.exist;
       expect(game.dbFile.endsWith('sqlite')).to.be.true;
     });
 
+    it("Should create a new folder for the created game", async function() {
+      const folder = game.getFolder();
+      expect(folder.includes('teerna')).to.be.true;
+    });
   });
 
   describe("Retrieve Game Sessions", function() {
-    it("Should not accept unauthenticated requests.");
 
-    it("Should create a new invitation.");
+    it("Should retrieve Game Sessions.", async () => {
+      const game2 = await GameSession.getSession({user_id: 'gm', email: 'gamemaster@example.com'}, game.guid);
+      expect(game.guid).to.equal(game2.guid);
+      expect(game.getFolder()).to.equal(game2.getFolder());
+      game2.deleteGame();
+    });
+  } );
 
-    it("Should return a list with the pending invitations.");
-    
+  describe("Game Invitations", function (){
+
+    it("Should be able to create invitations.", function () {
+      
+    });
+    it("Should be able to retrieve invitations.");
+    it("Invited users should be able to retrieve the game.");
+    it("Uninvited users should not be able to retrieve the game.");
   });
 
-  describe("GET guid", function (){
-    it("Should not accept unauthenticated requests");
-
-    it("Should not accept uninvited users");
-
-    it("Should remove accepted players from the invitation list");
-
-    it("Should acknowledge that the player was accepted.");
-  });
 
 });
 
